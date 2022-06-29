bf1062d0ecf0ad802253f431e1046fb1de8ea064
lucasluiz19 <lucas844@outlook.com.br>
2022-06-28 21:33:45 -0300

    finish-commit

--- script.js
+++ script.js
@@ -6,3 +6,38 @@ function clique(){
 
 }
 
+function exect(){
+   let result=document.getElementById('conteudo')
+   result.innerHTML='Dashboard'
+
+ }
+ function exect1(){
+   let result=document.getElementById('conteudo')
+   result.innerHTML='Pets'
+
+ }
+ function exect2(){
+   let result=document.getElementById('conteudo')
+   result.innerHTML='Clientes'
+
+ }
+ function exect3(){
+   let result=document.getElementById('conteudo')
+   result.innerHTML='Vets'
+
+ }
+ function exect4(){
+   let result=document.getElementById('conteudo')
+   result.innerHTML='Ajustes'
+
+ }
+
+ function sair(){
+   alert('Deseja realmente se desconectar?')
+ }
+
+ let btn = document.querySelector("#reload")
+   btn.addEventListener("click", function() {
+   
+    location.reload();
+});
